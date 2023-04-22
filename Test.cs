c1ca8176607d336d305a28108ffc6e65ba3955e7
ashishvaishya1010 <105185044+ashishvaishya1010@users.noreply.github.com>
2023-04-22 17:41:19 +0530

    Add files via upload

---
+++ Test.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Abc
+{
+class Test{
+// {static void Main(string[] args)
+//     {
+//         // Demo demo = new Demo();
+//         // Console.WriteLine(demo.getName());
+//         // Console.WriteLine(demo.getSum(10, 40));
+    
+// // int a=10;
+// // int b=20;
+// // int c=a+b;
+// // // int counter =0;
+// // // while (counter<10)
+// // // {
+// // System.Console.WriteLine(c);
+
+// //  for (int i=0;i<+20;i++){
+// //     Console.WriteLine(i);
+// //  }
+// var names = new List<string>{"Ashish","Krishna","Akshay","Prajkta"};
+
+// names.Add("Rushi");
+// names.Add("Shubham");
+// names.Remove("Ashish");
+
+// names.Sort();
+// foreach(string name in names){
+//     Console.WriteLine(name);
+// }
+
+// var index =  names.IndexOf("Prajkta");
+// Console.WriteLine(index);
+// // for (int i=0;i<names.Count;i++){
+// //      Console.WriteLine(names[i]);
+// // }
+
+    
+
+// }
+//     }}
+    
+}
+}
